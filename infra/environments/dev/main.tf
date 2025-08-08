c9c292edc5ed1e75475eae997ca3f553de3dbbce
pineapaul <poru.edirian@gmail.com>
2025-08-08 13:57:02 +1000

    Update infrastructure and deployment configuration

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -7,8 +7,8 @@ terraform {
   }
 
   backend "gcs" {
-    bucket  = "cycorgi-terraform-state"
-    prefix  = "env/dev"  # optional folder-like prefix in the bucket
+    bucket = "cycorgi-terraform-state"
+    prefix = "env/dev"
   }
 
   required_version = ">= 1.7.0"
@@ -20,6 +20,16 @@ variable "mongodb_uri" {
   sensitive   = true
 }
 
+variable "image_tag" {
+  description = "Docker image tag for the Cloud Run service"
+  type        = string
+}
+
+provider "google" {
+  project = "cycorgi-grc-platform"
+  region  = "us-central1"
+}
+
 resource "google_cloud_run_service" "grc_dev" {
   name     = "grc-dev"
   location = "us-central1"
@@ -27,10 +37,12 @@ resource "google_cloud_run_service" "grc_dev" {
   template {
     spec {
       containers {
-        image = "us-central1-docker.pkg.dev/cycorgi-grc-platform/grc-containers/grc-dev:latest"
+        image = "us-central1-docker.pkg.dev/cycorgi-grc-platform/grc-containers/grc-dev:${var.image_tag}"
+
         ports {
           container_port = 8080
         }
+
         env {
           name  = "MONGODB_URI"
           value = var.mongodb_uri
@@ -49,11 +61,6 @@ resource "google_cloud_run_service_iam_member" "public_access" {
   service  = google_cloud_run_service.grc_dev.name
   location = google_cloud_run_service.grc_dev.location
   project  = google_cloud_run_service.grc_dev.project
-  role   = "roles/run.invoker"
-  member = "serviceAccount:grc-dev-invoker@cycorgi-grc-platform.iam.gserviceaccount.com"
-}
-
-provider "google" {
-  project = "cycorgi-grc-platform"
-  region  = "us-central1"
+  role     = "roles/run.invoker"
+  member   = "serviceAccount:grc-dev-invoker@cycorgi-grc-platform.iam.gserviceaccount.com"
 }
